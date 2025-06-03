653801544de5f444e8422b699f69e6d8c33b1f65
Olúwafẹmi Love <42425031+oluwafemi-love@users.noreply.github.com>
2025-06-03 22:54:40 +0200

    wip: basic functionalities done

--- src/script.js
+++ src/script.js
@@ -1,9 +1,82 @@
+//ui
+const descriptionUI = document.getElementById("desc");
+const budgetAmountUI = document.getElementById("amount");
+
+const incomeBtn = document.getElementById("incomeBtn");
+const expensesBtn = document.getElementById("expenseBtn");
+
+const incomeListUL = document.getElementById("incomeList");
+const expenseListUL = document.getElementById("expenseList");
+const transactionListUL = document.getElementById("transactionList");
+
+const balanceUI = document.getElementById("balance");
+
+//data
+let description;
+let amount;
 let income = [];
 let expenses = [];
+let transaction = {};
 
-let descriptionUI = document.getElementById("desc");
-let budgetAmountUI = document.getElementById("amount");
+//Action
+incomeBtn.addEventListener("click", () => {
+  Defend();
+  AddIncome();
+  ClearUI();
+});
 
-let incomeBtn = document.getElementById("");
-let expensesBtn = document.getElementById("");
+expensesBtn.addEventListener("click", () => {
+  Defend();
+  AddExpenses();
+  ClearUI();
+});
+
+//Functionalities
+function AddIncome() {
+  //Retrieve Information
+  description = descriptionUI.value.trim();
+  amount = budgetAmountUI.value;
+
+  transaction = {
+    description: description,
+    amount: amount,
+    type: "income",
+  };
+
+  income.push(transaction);
+
+  console.log("This is the current transaction (income): ", transaction);
+  console.log("This is income: ", income);
+}
+
+function AddExpenses() {
+  //Retrieve Information
+  description = descriptionUI.value.trim();
+  amount = budgetAmountUI.value;
+
+  transaction = {
+    description: description,
+    amount: amount,
+    type: "expenses",
+  };
+
+  expenses.push(transaction);
+
+  console.log("This is the current transaction (expense): ", transaction);
+  console.log("This is expenses: ", expenses);
+}
+
+//Defend
+function Defend() {
+  if (!descriptionUI.value || !budgetAmountUI.value) {
+    alert("You need to input the needed information");
+  }
+}
+
+//Clear UI
+function ClearUI() {
+  descriptionUI.value = "";
+  budgetAmountUI.value = "";
+}
+
 
