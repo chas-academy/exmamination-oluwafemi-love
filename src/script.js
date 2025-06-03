20b8f07cc9468536d8ee33d4e137b787ba617c30
Olúwafẹmi Love <42425031+oluwafemi-love@users.noreply.github.com>
2025-06-03 15:12:21 +0200

    wip: css completed

---
+++ src/script.js
@@ -0,0 +1,9 @@
+let income = [];
+let expenses = [];
+
+let descriptionUI = document.getElementById("desc");
+let budgetAmountUI = document.getElementById("amount");
+
+let incomeBtn = document.getElementById("");
+let expensesBtn = document.getElementById("");
+
