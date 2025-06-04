fd4362ed1409a863a45694033e799550add83a00
Olúwafẹmi Love <42425031+oluwafemi-love@users.noreply.github.com>
2025-06-04 14:42:09 +0200

    wip: 2.0

--- src/script.js
+++ src/script.js
@@ -92,63 +92,54 @@ function ClearUI() {
 }
 
 function RenderIncome() {
-  let singleLi = document.createElement("li");
+  incomeListUL.innerHTML = ""; //clear previous list to avoid rendering issues
   for (icm of income) {
+    let singleLi = document.createElement("li");
     singleLi.innerHTML = `${icm.description}:  ${icm.amount}kr`;
     incomeListUL.appendChild(singleLi);
   }
 }
 
 function RenderExpenses() {
-  let singleLi = document.createElement("li");
+  expenseListUL.innerHTML = "";
   for (exp of expenses) {
+    let singleLi = document.createElement("li");
     singleLi.innerHTML = `${exp.description}:  ${exp.amount}kr`;
     expenseListUL.appendChild(singleLi);
   }
 }
 
 function RenderAllTransactions() {
-  let singleLi = document.createElement("li");
+  transactionListUL.innerHTML = "";
   for (allt of allTransactions) {
+    let singleLi = document.createElement("li");
     singleLi.innerHTML = `${allt.description}:  ${allt.amount}kr, Type: ${allt.type}`;
     transactionListUL.appendChild(singleLi);
   }
 }
 
 function TotalIncome() {
-  let totalIncome;
+  let totalIncome = 0;
   for (icm of income) {
-    totalIncome = 0;
-    console.log("income amount: ", icm.amount, "plus 1: ", icm.amount + 1);
     totalIncome += icm.amount;
   }
   return totalIncome;
 }
 
 function TotalExpenses() {
-  let totalExpenses;
+  let totalExpenses = 0;
   for (xpz of expenses) {
-    totalExpenses = 0;
-    console.log("expense amount: ", xpz.amount, "plus 1: ", xpz.amount + 1)
     totalExpenses += xpz.amount;
   }
   return totalExpenses;
 }
 
 function CalculateBalance() {
-  let balance = 0;
-  
-
-  if(!TotalIncome()){
-    balance = 0 - TotalExpenses();
-  }else if(!TotalExpenses()){
-    balance = TotalIncome() - 0;
-  }else if(!TotalIncome() && !TotalExpenses()){
-    balance = 0
-  }else if (TotalIncome() && TotalExpenses()){
-    balance +=  TotalIncome() - TotalExpenses();
-  }
-
+  const incomeTotal = TotalIncome();
+  const expenseTotal = TotalExpenses();
+  const balance = incomeTotal - expenseTotal;
   balanceUI.innerHTML = balance;
-  console.log(`Total Income: ${TotalIncome()}, Total Expenses: ${TotalExpenses()}, Balance: ${balance}`);
+  console.log(
+    `Total Income: ${TotalIncome()}, Total Expenses: ${TotalExpenses()}, Balance: ${balance}`
+  );
 }
